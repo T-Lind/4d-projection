13049b132455a1cc379a48d438fcad9249f1757b
TLindauer <tiernanxklindauer@gmail.com>
2024-05-24 16:58:29 -0500

    working intersection finder :)

--- 3d_test.py
+++ 3d_test.py
@@ -1,5 +1,5 @@
 import math
-
+from random import random as rand
 import pygame
 import sympy as sp
 from OpenGL.GL import *
@@ -18,9 +18,10 @@ glMatrixMode(GL_MODELVIEW)
 
 shape_points = [
     (1, 0, 0),
-    (0, 0, 1),
     (0, 1, 0),
+    (0, 0, 1),
     (0, 0, 0),
+    (2, 1, -1)
 ]
 
 shape_edges = [
@@ -30,24 +31,74 @@ shape_edges = [
     (3, 0),
     (0, 2),
     (1, 3),
+    (4, 0),
+    (4, 1),
+    (4, 3),
 ]
 plane_size = 2
 plane_x = 1
 plane_y = 0.5
 plane_angle = 0.65
-plane_points = [
-    (plane_x + plane_size * math.cos(plane_angle), 0.0, plane_y + plane_size * math.sin(plane_angle)),
-    (plane_x - plane_size * math.cos(plane_angle), 0.0, plane_y - plane_size * math.sin(plane_angle)),
-    (plane_x - plane_size * math.cos(plane_angle), 1.0, plane_y - plane_size * math.sin(plane_angle)),
-    (plane_x + plane_size * math.cos(plane_angle), 1.0, plane_y + plane_size * math.sin(plane_angle)),
-]
 
+plane_points = [
+    (plane_x + plane_size * math.cos(plane_angle), plane_y + plane_size * math.sin(plane_angle), -1),
+    (plane_x - plane_size * math.cos(plane_angle), plane_y - plane_size * math.sin(plane_angle), -1),
+    (plane_x - plane_size * math.cos(plane_angle), plane_y - plane_size * math.sin(plane_angle), 1),
+    (plane_x + plane_size * math.cos(plane_angle), plane_y + plane_size * math.sin(plane_angle), 1),
+]
 plane_edges = [
     (0, 1),
     (1, 2),
     (2, 3),
     (3, 0),
 ]
+
+
+def calculate_intersection(a, b, plane):
+    x1, y1, z1 = a
+    x2, y2, z2 = b
+    px, py = plane['x'], plane['y']
+    angle = plane['angle']
+
+    # Define symbolic variable
+    t = sp.symbols('t')
+
+    # Parametric equations of the line segment
+    x = x1 + t * (x2 - x1)
+    y = y1 + t * (y2 - y1)
+    z = z1 + t * (z2 - z1)
+
+    # Plane equation parameters
+    tan_theta = sp.tan(angle)
+
+    # Plane equation
+    plane_eq = y - (tan_theta * (x - px) + py)
+
+    # Solve for t
+    t_sol = sp.solve(plane_eq, t)
+
+    # Check if the solution lies within the segment
+    if t_sol:
+        t_val = t_sol[0]
+        if 0 <= t_val <= 1:
+            # Calculate intersection point
+            ix = x1 + t_val * (x2 - x1)
+            iy = y1 + t_val * (y2 - y1)
+            iz = z1 + t_val * (z2 - z1)
+            return float(ix), float(iy), float(iz)
+
+    return None
+
+intersections = []
+for edge in shape_edges:
+    a = shape_points[edge[0]]
+    b = shape_points[edge[1]]
+    intersection = calculate_intersection(a, b, {"x": plane_x, "y": plane_y, "angle": plane_angle})
+    if intersection:
+        intersections.append(intersection)
+
+print("Found intersections: ", intersections)
+
 # Camera position and rotation
 camera_x, camera_y, camera_z = 0.5, 0.5, 5.0
 camera_rotation_x, camera_rotation_y = 45, -45
@@ -56,6 +107,27 @@ camera_rotation_x, camera_rotation_y = 45, -45
 last_x, last_y = 0, 0
 mouse_down = False
 
+
+def draw_sphere(center, radius=0.025):
+    slices = 30
+    stacks = 30
+
+    glPushMatrix()
+    glTranslatef(center[0], center[1], center[2])
+    quadric = gluNewQuadric()
+    gluQuadricNormals(quadric, GLU_SMOOTH)
+    gluQuadricTexture(quadric, GL_TRUE)
+    gluSphere(quadric, radius, slices, stacks)
+    gluDeleteQuadric(quadric)
+    glPopMatrix()
+
+def draw_shape(points, edges, color=(1.0, 1.0, 1.0)):
+    glBegin(GL_LINES)
+    glColor3f(*color)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(points[vertex])
+    glEnd()
 
 def draw():
     global camera_x, camera_y, camera_z, camera_rotation_x, camera_rotation_y
@@ -67,20 +139,13 @@ def draw():
     glRotatef(camera_rotation_x, 1, 0, 0)
     glRotatef(camera_rotation_y, 0, 1, 0)
 
-    # Render the shape
-    glBegin(GL_LINES)
-    glColor3f(1.0, 1.0, 1.0)
-    for edge in shape_edges:
-        for vertex in edge:
-            glVertex3fv(shape_points[vertex])
-    glEnd()
+    draw_shape(shape_points, shape_edges)
+    draw_shape(plane_points, plane_edges, (1.0, 0.0, 0.0))
 
-    glColor3f(1.0, 0.0, 0.0)
-    glBegin(GL_LINES)
-    for edge in plane_edges:
-        for vertex in edge:
-            glVertex3fv(plane_points[vertex])
-    glEnd()
+    for intersection in intersections:
+        glColor3f(rand(), rand(), rand())
+        draw_sphere(intersection)
+
     pygame.display.flip()
 
 
@@ -127,51 +192,7 @@ while running:
 
 pygame.quit()
 
-
-def calculate_intersection(a, b, plane):
-    x1, y1, z1 = a
-    x2, y2, z2 = b
-    px, py = plane['x'], plane['y']
-    angle = plane['angle']
-
-    # Define symbolic variable
-    t = sp.symbols('t')
-
-    # Parametric equations of the line segment
-    x = x1 + t * (x2 - x1)
-    y = y1 + t * (y2 - y1)
-    z = z1 + t * (z2 - z1)
-
-    # Plane equation parameters
-    tan_theta = sp.tan(angle)
-
-    # Plane equation
-    plane_eq = y - (tan_theta * (x - px) + py)
-
-    # Solve for t
-    t_sol = sp.solve(plane_eq, t)
-
-    # Check if the solution lies within the segment
-    if t_sol:
-        t_val = t_sol[0]
-        if 0 <= t_val <= 1:
-            # Calculate intersection point
-            ix = x1 + t_val * (x2 - x1)
-            iy = y1 + t_val * (y2 - y1)
-            iz = z1 + t_val * (z2 - z1)
-            return float(ix), float(iy), float(iz)
-
-    return None
-
-
 # print(calculate_intersection((1, 2, 2), (-1, 0, 0), {"x": -1.4, "y": 0.3, "angle": 0.65}))
 # find points of intersection between the plane and the shape
-intersections = []
-for edge in shape_edges:
-    a = shape_points[edge[0]]
-    b = shape_points[edge[1]]
-    intersection = calculate_intersection(a, b, {"x": plane_x, "y": plane_y, "angle": plane_angle})
-    if intersection:
-        intersections.append(intersection)
 
-print(intersections)
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# 4D to 3D Mapping
+Hey there!
+
+This project allows you to construct 4D objects and then view hyperplanes or "3D slices" of the 4D world.
+
+This could be used for all sorts of mind-bending visualizations, and could be easily replicated into C++ or another more performant language.
